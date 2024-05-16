851b858e688c0af97c955276da973bb7ab666d98
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-05-16 14:14:23 +0300

    OK-496: Lisätty CI-putki, joka lataa storybookin S3:een

---
+++ scripts/deploy-storybook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e pipefail
+
+echo "Copying Storybook files to AWS S3 buildnumber (ga-$GITHUB_RUN_NUMBER) directory"
+aws s3 sync storybook-static s3://dev-files.ops.opintopolku.fi/storybooks/oph-design-system/builds/ga-$GITHUB_RUN_NUMBER --delete --cache-control max-age=600 --acl public-read
+echo "Storybook link (temporary for build): https://dev-files.ops.opintopolku.fi/storybooks/oph-design-system/builds/ci-$GITHUB_RUN_NUMBER/index.html"
+
+echo "Copying Storybook files to AWS S3 branch ($GITHUB_REF_NAME) directory"
+aws s3 sync storybook-static s3://dev-files.ops.opintopolku.fi/storybooks/oph-design-system/$GITHUB_REF_NAME --delete --cache-control max-age=600 --acl public-read
+echo "Storybook link (permanent for branch): https://dev-files.ops.opintopolku.fi/storybooks/oph-design-system/$GITHUB_REF_NAME/index.html"
