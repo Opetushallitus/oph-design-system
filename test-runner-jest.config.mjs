fcdc3afbaf403b8d11fc8f78c1daa7a6e39b001b
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-05-22 09:55:25 +0300

    OK-496: Disabloidaan fonttien antialisointi kun ajetaan test-storybook

---
+++ test-runner-jest.config.mjs
@@ -0,0 +1,22 @@
+import { getJestConfig } from '@storybook/test-runner';
+// The default Jest configuration comes from @storybook/test-runner
+const testRunnerConfig = getJestConfig();
+
+export default {
+  ...testRunnerConfig,
+  testEnvironmentOptions: {
+    ...(testRunnerConfig?.testEnvironmentOptions ?? {}),
+    'jest-playwright': {
+      ...(testRunnerConfig?.testEnvironmentOptions?.['jest-playwright'] ?? {}),
+      launchOptions: {
+        ...(testRunnerConfig?.testEnvironmentOptions?.['jest-playwright']?.launchOptions ?? {}),
+        args: [
+          '--font-render-hinting=none',
+          '--disable-skia-runtime-opts',
+          '--disable-font-subpixel-positioning',
+          '--disable-lcd-text',
+        ],
+      },
+    },
+  },
+};
