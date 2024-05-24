d78685bce5f27fa606e64a8e4606003dff259b66
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-05-24 15:35:23 +0300

    OK-496: Karsittu Button-komponentin propseja ja lisätty forwardRef

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,11 +1,35 @@
 import type { ButtonProps as MuiButtonProps } from '@mui/material';
 import MuiButton from '@mui/material/Button';
+import { forwardRef } from 'react';
 
-export interface ButtonProps
-  extends Omit<MuiButtonProps, 'endIcon' | 'startIcon' | 'size'> {
-  icon?: MuiButtonProps['startIcon'];
-}
+const PROPS_FROM_MUI_BUTTON = [
+  'action',
+  'classes',
+  'className',
+  'children',
+  'disabled',
+  'focusVisibleClassName',
+  'href',
+  'onClick',
+  'onFocusVisible',
+  'ref',
+  'startIcon',
+  'style',
+  'sx',
+  'tabIndex',
+  'variant',
+] as const;
 
-export const Button = ({ icon, ...rest }: ButtonProps) => {
-  return <MuiButton {...rest} startIcon={icon} />;
-};
+export type ButtonProps = Pick<
+  MuiButtonProps,
+  (typeof PROPS_FROM_MUI_BUTTON)[number]
+>;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button(props, ref) {
+    const filteredProps = Object.fromEntries(
+      PROPS_FROM_MUI_BUTTON.map((propName) => [propName, props[propName]]),
+    );
+    return <MuiButton {...filteredProps} ref={ref} />;
+  },
+);
