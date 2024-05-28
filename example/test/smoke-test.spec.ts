869fdef47ba2cbe62328fd67a35d140fded4226a
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-05-28 14:09:18 +0300

    OK-496: Lisätty savutesti esimerkkiprojektille
    - Testataan, että kirjasto saadaan importoitua ja komponenttien käyttö onnistuu.

---
+++ example/test/smoke-test.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Example renders', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  const heading = page.getByRole('heading', {name: 'Otsikko'});
+  const button = page.getByRole('button', {name: 'Painike'});
+
+  await expect(heading).toBeVisible();
+  await expect(button).toBeVisible();
+});
