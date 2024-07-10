66d34f9ae004e2c1e0b2036e114d53cd2883c891
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-07-10 08:21:23 +0300

    OK-513: Paranneltu Button-komponenttia

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,63 +1,48 @@
-import { toCompactArray } from './util';
 import {
   Button as MuiButton,
   type ButtonProps as MuiButtonProps,
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { forwardRef } from 'react';
-
-const PROPS_FROM_MUI_BUTTON = [
-  'action',
-  'classes',
-  'className',
-  'children',
-  'disabled',
-  'focusVisibleClassName',
-  'href',
-  'onClick',
-  'onFocusVisible',
-  'ref',
-  'startIcon',
-  'style',
-  'sx',
-  'tabIndex',
-  'variant',
-] as const;
 
 export type ButtonProps = Pick<
   MuiButtonProps,
-  (typeof PROPS_FROM_MUI_BUTTON)[number]
+  | 'action'
+  | 'classes'
+  | 'className'
+  | 'children'
+  | 'disabled'
+  | 'focusVisibleClassName'
+  | 'href'
+  | 'onClick'
+  | 'onFocusVisible'
+  | 'ref'
+  | 'startIcon'
+  | 'style'
+  | 'sx'
+  | 'tabIndex'
+  | 'variant'
 >;
 
+const StyledButton = styled(MuiButton)(({ theme, children }) => {
+  return children
+    ? {}
+    : {
+        //Jos ei lapsia, pienennetään marginaaleja, jotta pelkkä ikoni näkyy hyvin
+        '&.MuiButton-root': {
+          padding: theme.spacing(0.5),
+          margin: 0,
+          minWidth: 0,
+          flexShrink: 0,
+        },
+        '& .MuiButton-startIcon': {
+          margin: 0,
+        },
+      };
+});
+
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  function Button(props, ref) {
-    const filteredProps = Object.fromEntries(
-      PROPS_FROM_MUI_BUTTON.map((propName) => [propName, props[propName]]),
-    );
-
-    const { sx } = filteredProps;
-
-    return (
-      <MuiButton
-        {...props}
-        ref={ref}
-        sx={[
-          filteredProps.children
-            ? {}
-            : {
-                //Jos ei lapsia, pienennetään marginaaleja, jotta pelkkä ikoni näkyy hyvin
-                '&.MuiButton-root': {
-                  padding: 0.5,
-                  margin: 0,
-                  minWidth: 0,
-                  flexShrink: 0,
-                },
-                '& .MuiButton-startIcon': {
-                  margin: 0,
-                },
-              },
-          ...toCompactArray(sx),
-        ]}
-      />
-    );
+  function renderButton(props, ref) {
+    return <StyledButton {...props} ref={ref} />;
   },
 );
