f3446727c17f99868f37ab8319b2927349d044b3
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-08-22 10:15:24 +0300

    Päivitetty riippuvuudet ja korjattu eslint-konffit

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -23,9 +23,9 @@ declare module '@mui/material/styles' {
     label?: React.CSSProperties;
   }
 
-  /* eslint-disable-next-line @typescript-eslint/no-empty-interface */
+  /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
   interface TypographyVariants extends CustomTypographyVariants {}
-  /* eslint-disable-next-line @typescript-eslint/no-empty-interface */
+  /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
   interface TypographyVariantsOptions extends CustomTypographyVariants {}
 
   interface BreakpointOverrides {
