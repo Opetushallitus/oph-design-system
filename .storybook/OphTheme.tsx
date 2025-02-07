9296677a8aabe89b32611f29edb2545df79a00c8
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-02-07 10:50:18 +0200

    Parannettu storybookin teemaa

---
+++ .storybook/OphTheme.tsx
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'OPH Design System Storybook',
+  brandUrl: 'https://oph.fi',
+  brandImage: '/assets/oph_logo.png',
+  brandTarget: '_self',
+});
