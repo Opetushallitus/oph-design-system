5a14b6cab3e675b1961a21ad31a82591f3dde850
Joona Haavisto <joona.haavisto@gofore.com>
2025-08-29 13:15:57 +0300

    Storyt multi selectille

---
+++ src/components/OphSelectMultipleFormField.stories.tsx
@@ -0,0 +1,34 @@
+import { OphSelectMultipleFormField } from '@/src';
+import type { Meta, StoryObj } from '@storybook/react';
+import { OPTIONS } from '../story-common';
+
+const meta = {
+  component: OphSelectMultipleFormField,
+  parameters: {
+    viewport: {
+      defaultViewport: 'desktop1',
+    },
+  },
+} satisfies Meta<typeof OphSelectMultipleFormField>;
+
+export default meta;
+
+type Story = StoryObj<typeof OphSelectMultipleFormField>;
+
+export const Default: Story = {
+  args: {
+    options: OPTIONS,
+    placeholder: 'Select',
+    inputProps: {
+      'aria-label': 'select',
+    },
+  },
+};
+
+export const ValuesSelected: Story = {
+  ...Default,
+  args: {
+    value: ['1', '2'],
+    ...Default.args,
+  },
+};
