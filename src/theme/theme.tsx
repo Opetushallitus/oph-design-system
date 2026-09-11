203d4712a8abdeaa7f7017d140b9e0294373401e
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-09-11 11:23:29 +0300

    Cleaned up redundant style declarations

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -93,19 +93,14 @@ const COMMON_THEME_OPTIONS: ThemeOptions = {
                   borderColor: variantTheme.palette.primary.main,
                   '&.Mui-disabled': {
                     color: ophColors.grey400,
-                    borderWidth: '1px',
                     borderColor: ophColors.grey400,
                   },
                   '&:hover': {
-                    backgroundColor: ophColors.white,
                     color: variantTheme.palette.primary.light,
-                    borderWidth: '1px',
                     borderColor: variantTheme.palette.primary.light,
                   },
                   '&:active': {
-                    backgroundColor: ophColors.white,
                     color: variantTheme.palette.primary.dark,
-                    borderWidth: '1px',
                     borderColor: variantTheme.palette.primary.dark,
                   },
                 };
